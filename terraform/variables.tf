c0e6d51a8707e33cde5d422e0cf70d176958fd95
Laura Bermudez <laurab1528@hotmail.com>
2025-06-10 18:46:17 -0500

    update infraestructure

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,6 @@
 variable "aws_region" {
   description = "Región de AWS"
   type        = string
-
-}
-
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-  type        = string
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-  type        = string
 }
 
 variable "api_key" {
